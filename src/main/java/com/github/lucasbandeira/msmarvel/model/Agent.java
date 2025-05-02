4e570b3f2ce6a926101486b023df445d165870d3
Lucas Bandeira <lucasbandeirai40@gmail.com>
2025-05-02 16:25:41 -0300

    feat: add basic auth credentials to Eureka defaultZone for secure registration

--- src/main/java/com/github/lucasbandeira/msmarvel/model/Agent.java
+++ src/main/java/com/github/lucasbandeira/msmarvel/model/Agent.java
@@ -4,6 +4,7 @@ import com.github.lucasbandeira.msmarvel.model.dto.AgentRequestDTO;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
@@ -11,6 +12,7 @@ import java.util.UUID;
 @Table(name = "tb_agent")
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Agent {
 
     @Id
